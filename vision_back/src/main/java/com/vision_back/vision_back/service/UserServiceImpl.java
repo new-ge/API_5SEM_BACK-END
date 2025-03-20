9b583ac5c9d371fd0a29d9515f5f64f5f68d6efb
Naiara Santos <nai.leticialemos31@gmail.com>
2025-03-19 23:14:10 -0300

    VSN-17 - fix: adjust return/print to correctly display IDs from test JSON

--- vision_back/src/main/java/com/vision_back/vision_back/service/UserServiceImpl.java
+++ vision_back/src/main/java/com/vision_back/vision_back/service/UserServiceImpl.java
@@ -6,13 +6,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vision_back.vision_back.VisionBackApplication;
 
 public class UserServiceImpl {
-    public String getUserId() {
+    public List<String> getUserId() {
+
         ObjectMapper objectMapper = new ObjectMapper();
         RestTemplate restTemplate = new RestTemplate();
         VisionBackApplication vba = new VisionBackApplication();
@@ -25,24 +28,35 @@ public class UserServiceImpl {
         ResponseEntity<String> response = restTemplate.exchange("https://api.taiga.io/api/v1/users?project=1641986", HttpMethod.GET, headersEntity, String.class);
            
         try {
-            if (response.getBody().contains("id")) {
-                System.out.print(response);
-                JsonNode jsonNode = objectMapper.readTree(response.getBody());
-                JsonNode getUserId = jsonNode.get("id");
-                System.out.println(jsonNode.get("id"));
-                return new ObjectMapper().writeValueAsString(getUserId).replace("\"", "");
-            } else {
-                throw new NullPointerException("Erro 404: Resposta não obtida.");
-            }
-        } catch (Exception e) {
-            return e.getMessage();
+            System.out.print("Retorno da API: " + response.getBody()); //Anotação apenas para verificação
+
+            JsonNode JsonNode = objectMapper.readTree(response.getBody());
+
+            //Elaboração da lista para armazenar todos os id's
+            List<String> userIds = new ArrayList<>();
+
+            if(JsonNode.isArray()){
+                for (JsonNode user : JsonNode){
+                    JsonNode idNode = user.get("id");
+                    if (idNode != null){
+                        String userId =idNode.asText();
+                        System.out.println("ID extraída: " + userId);
+                        userIds.add(userId);
+                    }
+                    
+                }
+                return userIds;
+            } else{
+                System.out.println("Erro: Resposta não é uma Lista. ");
+                return new ArrayList<>();
+            } 
+        }catch (Exception e) {
+                System.out.println("Erro ao processar a resposta da API: " + e.getMessage());
+                return new ArrayList<>();}
         }
-    }
-
-
+    
     
 } 
-    
 
 
 //interface
