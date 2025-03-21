2b6a712ab7c1b4627ee10076177f86b3877617fa
ChavesVini <vinidasilva2004@outlook.com>
2025-03-21 19:47:29 -0300

    VSN-18 fix: resolve SonarQube issues related to null checks

--- vision_back/src/main/java/com/vision_back/vision_back/service/ProjectServiceImpl.java
+++ vision_back/src/main/java/com/vision_back/vision_back/service/ProjectServiceImpl.java
@@ -7,13 +7,14 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vision_back.vision_back.VisionBackApplication;
 
 public class ProjectServiceImpl {
 
-    public String getProjectId(String slugProject) {
+    public String getProjectId(String slugProject) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         RestTemplate restTemplate = new RestTemplate();
         VisionBackApplication vba = new VisionBackApplication();
@@ -24,17 +25,13 @@ public class ProjectServiceImpl {
             
         HttpEntity<Void> headersEntity = new HttpEntity<>(headers);
         ResponseEntity<String> response = restTemplate.exchange("https://api.taiga.io/api/v1/projects/by_slug?slug="+slugProject, HttpMethod.GET, headersEntity, String.class);
-            
+    
         try {
-            if (response.getBody().contains("id")) {
-                JsonNode jsonNode = objectMapper.readTree(response.getBody());
-                JsonNode getProjectId = jsonNode.get("id");
-                return new ObjectMapper().writeValueAsString(getProjectId).replace("\"", "");
-            } else {
-                throw new NullPointerException("Erro 404: Resposta não obtida.");
-            }
+            JsonNode jsonNode = objectMapper.readTree(response.getBody());
+            JsonNode getProjectId = jsonNode.get("id");
+            return new ObjectMapper().writeValueAsString(getProjectId).replace("\"", "");
         } catch (Exception e) {
-            return e.getMessage();
+            throw new NullPointerException("Resposta não obtida ou resposta inválida.");
         }
     }
 }
