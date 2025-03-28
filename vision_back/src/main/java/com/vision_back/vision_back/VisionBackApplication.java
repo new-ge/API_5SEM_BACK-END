0c4d5ab472fdd2ffdbf3eaaa79c5a29c6639838c
ChavesVini <vinidasilva2004@outlook.com>
2025-03-27 22:32:05 -0300

    VSN-71 fix: removing an useless method

--- vision_back/src/main/java/com/vision_back/vision_back/VisionBackApplication.java
+++ vision_back/src/main/java/com/vision_back/vision_back/VisionBackApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.vision_back.vision_back.service.AuthenticationServiceImpl;
-import com.vision_back.vision_back.service.UserServiceImpl;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
@@ -20,11 +19,5 @@ public class VisionBackApplication {
 	public String functionGetToken() {
 		AuthenticationServiceImpl auth = new AuthenticationServiceImpl();
 		return auth.getTokenAuthentication(dotenv.get("PASSWORD_SECRET"), dotenv.get("USERNAME_SECRET"));
-	}
-
-	@PostConstruct
-	public void run() {
-		UserServiceImpl usImpl = new UserServiceImpl();
-		System.out.println(usImpl.getUserId(1641986));
 	}
 }
