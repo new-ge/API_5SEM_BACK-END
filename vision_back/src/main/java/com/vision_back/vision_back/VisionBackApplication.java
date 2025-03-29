284f436f929e93427c17c848538e44b3dad0acc3
Cauan Barbaglio <cauanvmb15@gmail.com>
2025-03-28 21:51:11 -0300

    chore: Remove unnecessary imports

--- vision_back/src/main/java/com/vision_back/vision_back/VisionBackApplication.java
+++ vision_back/src/main/java/com/vision_back/vision_back/VisionBackApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.vision_back.vision_back.service.AuthenticationServiceImpl;
-import com.vision_back.vision_back.service.ProjectServiceImpl;
-import com.vision_back.vision_back.service.UserStoryServiceImpl;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
