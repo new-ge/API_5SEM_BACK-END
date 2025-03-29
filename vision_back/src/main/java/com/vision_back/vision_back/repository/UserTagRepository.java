ef4f1dc03a19b9cb590e94c15a1b672dcda2f5e7
BahijJNoureddine <bahij.noureddine@fatec.sp.gov.br>
2025-03-29 20:08:19 -0300

    VSN-15 - feat: add methods repository for fetching user tag data
    - Implemented new repository methods to retrieve user tag data, such as fetching card count by tag and other relevant user-tag metrics.
    - These methods improve the interaction with the database, allowing more efficient querying of user and tag relationships.
    - The new methods ensure data consistency and provide necessary support for the feature of calculating card counts by tag.

--- vision_back/src/main/java/com/vision_back/vision_back/repository/UserTagRepository.java
+++ vision_back/src/main/java/com/vision_back/vision_back/repository/UserTagRepository.java
@@ -1,9 +1,14 @@
 package com.vision_back.vision_back.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.vision_back.vision_back.entity.UserTagEntity;
 
 public interface UserTagRepository extends JpaRepository<UserTagEntity,Integer>{
+    @Query("SELECT ut.tag.id, SUM(ut.quant) FROM UserTag ut GROUP BY ut.tag.id")
+    List<Object[]> countCardsByTag();
     
 }
